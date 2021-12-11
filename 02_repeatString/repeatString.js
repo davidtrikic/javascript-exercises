7a65a7a7ded955c06c6055c29b3874235bc6add4
david <davidtrikic@outlook.com>
2021-12-11 22:51:09 +0100

    solve exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let outStr = "";
+  if (num < 0) return "ERROR";
+  for (let i = 0; i < num; i++) {
+    outStr += str;
+  }
+  return outStr;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...nums) {
+  for (let n of nums) {
+    if (array.indexOf(n) === -1) continue;
+    array.splice(array.indexOf(n), 1);
+  }
+  return array;
 };
 
 // Do not edit below this line
